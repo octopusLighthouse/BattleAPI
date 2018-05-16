be2ba396bf9d9b1649b03821159abd282fb2073e
ROBO <robertas.bauras@gmail.com>
2018-05-16 11:13:04 +0300

    Initial

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -1,4 +1,3 @@
 const mongoose = require('mongoose');
-//mongoose.connect('mongodb://akis:abcabc@ds113200.mlab.com:13200/octopus');
 mongoose.connect('mongodb://playerX:Vilnius_1983@ds111390.mlab.com:11390/game');
 module.exports = mongoose;
--- models/battles.js
+++ models/battles.js
@@ -1,20 +1,4 @@
 var mongoose = require('./../libs/mongoose');
-//{"longitude": "123456", "latitude": "987654", "comment": "Pagavau zuvi negaudes", "fishName": "Maximos karpis", "fishImage": "www.maxima.lt/nabagas/karpis.jpg"}
-
-/*
-exports.Record = mongoose.model(
-	'fishers', 
-	{longitude: String, latitude: String, comment: String, fishName: String, fishImage: String}, 
-	'fishers');
-	*/
-
-/*
-exports.FISHERS = mongoose.model(
-	'fishers', 
-	{longitude: String, latitude: String, comment: String, fishName: String, fishImage: String}, 
-	'fishers');
-*/
-
 
 exports.battles = mongoose.model(
 	'battles', 
