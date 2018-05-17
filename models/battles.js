bcc290b41841ccbe8a8a932508bcd72e432a7fdf
ROBO <robertas.bauras@gmail.com>
2018-05-17 20:12:09 +0300

    Initial

--- models/battles.js
+++ models/battles.js
@@ -1,32 +1,11 @@
+// --------------------------------------------------------------------------------------------
+// Description: Task
+// Author: Robertas Bauras
+// Create date: 2018.05.02
+// Modify date: 2018.05.16
+// --------------------------------------------------------------------------------------------
 var mongoose = require('./../libs/mongoose');
-
-exports.battles = mongoose.model(
-	'battles', 
-	{
-	    name: String,
-	    year: Number,
-	    battle_number: Number,
-	    attacker_king: String,
-	    defender_king: String,
-	    attacker_1: String,
-	    attacker_2: String,
-	    attacker_3: String,
-	    attacker_4: String,
-	    defender_1: String,
-	    defender_2: String,
-	    defender_3: String,
-	    defender_4: String,
-	    attacker_outcome: String,
-	    battle_type: String,
-	    major_death: Number,
-	    major_capture: Number,
-	    attacker_size: Number,
-	    defender_size: Number,
-	    attacker_commander: String,
-	    defender_commander: String,
-	    summer: Number,
-	    location: String,
-	    region: String,
-	    note: String
-	}, 
-	'battles');
+var schema = require('./../schemas/battles');
+// --------------------------------------------------------------------------------------------
+exports.battles = mongoose.model('battles', schema.battles, 'battles');
+// --------------------------------------------------------------------------------------------
