95938c3630438142dffd53b55260db3f51ac5c7f
ROBO <robertas.bauras@gmail.com>
2018-05-18 10:05:39 +0300

    Initial

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://playerX:---------@ds111390.mlab.com:11390/game');
+mongoose.connect('mongodb://playerX:Vilnius_1983@ds111390.mlab.com:11390/game');
 module.exports = mongoose;
